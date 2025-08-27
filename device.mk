c8c24a14e88f6e3f6555723a684fb00322f546ee
Musafir02 <askibrahim06@gmail.com>
2025-08-27 06:17:23 +0000

    sweet: Enable Dolby

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-sweet/device.mk)
 
+# Dolby
+TARGET_USES_DOLBY := true
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
