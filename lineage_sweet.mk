b65fe461803ed575e01464cb39ccc7c9276cd787
Musafir02 <askibrahim06@gmail.com>
2025-08-27 11:25:41 +0000

    Revert "sweet: RisingOS Revived bringup"
    This reverts commit 73e7e1efe76b7002f6ea5537d8a4846aa2a51fab.

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -25,26 +25,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys" \
-    BuildFingerprint=Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys \
-    DeviceProduct=sweet \
-    RisingMaintainer="Musafir02" \
-    RisingChipset="Snapdragon 732G"
-
-## risingOS-Flags
-# Addons
-TARGET_HAS_UDFPS := false
-
-# Gms 
-WITH_GMS := true
-TARGET_CORE_GMS := true
-TARGET_CORE_GMS_EXTRAS := true
-
-# Ship Pixel Launcher
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-
-#blur
-TARGET_ENABLE_BLUR := true
-
-# rising Maintainer
-RISING_MAINTAINER := Musafir02
+    BuildFingerprint=Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
