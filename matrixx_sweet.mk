7fa082573db04779a001f31329518ba243ca34cd
Musafir02 <askibrahim06@gmail.com>
2026-04-29 06:18:59 +0000

    sweet: Project Matrixx Bringup

--- matrixx_sweet.mk
+++ matrixx_sweet.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Matrixx stuff.
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := matrixx_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -26,3 +26,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys" \
     BuildFingerprint=Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
+
+MATRIXX_MAINTAINER := Musafir02
+TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
+WITH_GMS := true
+WITH_GMS_AICORE := true
+TARGET_OPTIMIZED_DEXOPT := true
+BYPASS_CHARGE_SUPPORTED := true
+HBM_SUPPORTED := false
+HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
